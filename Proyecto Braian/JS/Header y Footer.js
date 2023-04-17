300c47514959753875b0f9171d44ccd268379bc2
DorregoBraian <rayan_22_dorrego@hotmail.com>
2023-04-17 09:48:17 -0300

    en el html de recetas de comidas puse una etiqueta nav con 3 filtros y me tage la list de la api. ademas solucione el problema de las card de resetas

--- Proyecto Braian/JS/Header y Footer.js
+++ Proyecto Braian/JS/Header y Footer.js
@@ -5,6 +5,9 @@ $(document).ready(function () {
     $("#footer").html(Footer());
     Receta();
     listaDeRecetas();
+    filtroPais();
+    filtroIngrediente();
+    filtroCategoria();
 })
 
 
@@ -66,18 +69,116 @@ const listaDeRecetas = () => {
             console.log(datos);
 
             $.each(datos, function (index, elemento) {
-                console.log(elemento[0].strMeal);
+                console.log(elemento);
 
-                //$(".nombre-receta").text(elemento[0].strMeal);
-                //$(".imagen-receta").attr("src", elemento[0].strMealThumb);
-                //$(".descipcion-receta").text(elemento[0].strInstructions);
-                $(".section-card").append(`
-                <img src= ${elemento[0].strMealThumb} alt="">
-                <div class="div-card">
-                    <h3>${elemento[0].strMeal}</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur distinctio in deserunt incidunt aliquam fugit ullam nulla repudiandae mollitia pariatur, ratione placeat enim reiciendis. Eum doloribus necessitatibus voluptate corporis ipsam.</p>
-                </div>`);
+                $(".nombre-receta").text(elemento[0].strMeal);
+                $(".imagen-receta").attr("src", elemento[0].strMealThumb);
+                $(".descipcion-receta").text(elemento[0].strInstructions);
+
+                for (var index = 0; index <= elemento.length; index++) {
+                    $("#main").append(`
+                    <a href="/HTML/Reseta.html">
+                    <section class="section-card">
+                        <img src= "${elemento[index].strMealThumb}" alt="">
+                        <div class="div-card">
+                            <h3>${elemento[index].strMeal}</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur distinctio in deserunt incidunt aliquam fugit ullam nulla repudiandae mollitia pariatur, ratione placeat enim reiciendis. Eum doloribus necessitatibus voluptate corporis ipsam.
+                            <br>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur distinctio in deserunt incidunt aliquam fugit ullam nulla repudiandae mollitia pariatur, ratione placeat enim reiciendis. Eum doloribus necessitatibus voluptate corporis ipsam.</p>
+                        </div>
+                    </section></a>
+                    `); 
+                }
+            });
+        
+
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+        }
+    })
+}
+
+const filtroPais = () => {
+    var url = `https://www.themealdb.com/api/json/v1/1/list.php?a=list`;
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        success: function (datos) {
+
+            console.log(datos);
+
+            $.each(datos, function (index, pais) {
+                console.log(pais[0].strArea);
                 
+                for (var index = 0; index <= pais.length; index++) {
+                    $("#filtro-pais").append(`
+                <option value="${pais[index].strArea}">${pais[index].strArea}</option>
+                `);
+                }
+            });
+        
+
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+        }
+    })
+}
+
+const filtroIngrediente = () => {
+    var url = `https://www.themealdb.com/api/json/v1/1/list.php?i=list`;
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        success: function (datos) {
+
+            console.log(datos);
+
+            $.each(datos, function (index, ingredientes) {
+                console.log(ingredientes[0].strIngredient);
+                
+                for (var index = 0; index <= ingredientes.length; index++) {
+                    $("#filtro-ingrediente").append(`
+                <option value="${ingredientes[index].strIngredient}">${ingredientes[index].strIngredient}</option>
+                `);
+                }
+            });
+        
+
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+        }
+    })
+}
+
+const filtroCategoria = () => {
+    var url = `https://www.themealdb.com/api/json/v1/1/list.php?c=list`;
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        success: function (datos) {
+
+            console.log(datos);
+
+            $.each(datos, function (index, categoria) {
+                console.log(categoria[0].strCategory);
+                
+                for (var index = 0; index <= categoria.length; index++) {
+                    $("#filtro-categoria").append(`
+                <option value="${categoria[index].strCategory}">${categoria[index].strCategory}</option>
+                `);
+                }
             });
         
 
