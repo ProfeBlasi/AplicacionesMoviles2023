31d4324242254de860c604516a19ff7c1a7f63ad
Blasi Eduardo <eblasi@andreani.com>
2023-04-25 15:15:57 -0300

    Se finaliza el desarrollo de page formulario

--- js/script.js
+++ js/script.js
@@ -1,45 +1,59 @@
 import { maquetarHome } from "./pages/home.js"
 import { maquetarReceta } from "./pages/receta.js"
-
-const opciones = {
-  'Home': '../html/home.html',
-  'RecetaDe': '../html/recetaComida.html',
-  'Noticias': '../html/noticias.html',
-  'Formulario': '../html/formulario.html',
-};
+import { createMap } from "./pages/formulario.js"
 
 const $main = $('#main-cointaner');
+  const opciones = {
+    'Home': '../html/home.html',
+    'RecetaDe': '../html/recetaComida.html',
+    'Noticias': '../html/noticias.html',
+    'Contacto': '../html/contacto.html',
+  };
 
-function cargarVista(vistaUrl) {
-  var vista = vistaUrl.substring(1);
-  $main.load(opciones[vista]);
-  switch (vista) {
-    case "Home":
-      maquetarHome();
-      break;
-    case "RecetaDe":
-      maquetarReceta();
-      break;
-    case "Noticias":
-      console.log("El color es verde");
-      break;
-    case "Formulario":
-      console.log("El color es azul");
-      break;
+  function cargarVista(vistaUrl) {
+    var vista = vistaUrl.substring(1);
+    $main.load(opciones[vista],function() {
+      switch (vista) {
+        case "Home":
+          maquetarHome();
+          break;
+        case "RecetaDe":
+          maquetarReceta();
+          break;
+        case "Contacto":
+          createMap();
+          break;
+      }
+    });
   }
-}
 
-$('.menu a').click(function (event) {
-  event.preventDefault();
-  var page = $(this).attr('href');
-  cargarVista(page);
-});
+  $('.menu a').click(function (event) {
+    event.preventDefault();
+    var page = $(this).attr('href');
+    cargarVista(page);
+  });
 
-$('.mobile-menu').change(function () {
-  var page = $(this).val();
-  cargarVista(page);
-});
+  $('.mobile-menu').change(function () {
+    var page = $(this).val();
+    cargarVista(page);
+  });
+
+  $(document).ready(function () {
+    cargarVista('#Home');
+  });
 
 $(document).ready(function () {
-  cargarVista('#Home');
+  $("#enviar").click(function (event) {
+    var error = false;
+    $(".error").text("");
+    $("#formulario input[required], #formulario textarea[required]").each(function () {
+      if ($(this).val() == "") {
+        $(this).next(".error").text("Este campo es obligatorio");
+        error = true;
+      }
+    });
+    if (error) {
+      event.preventDefault();
+    }
+  });
 });
